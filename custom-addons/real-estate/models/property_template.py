e992372dd65f2873dae03541709c9402bd1431b9
DucQuoc <21522527@gm.uit.edu.vn>
2024-12-06 03:05:48 +0700

    update[real-estate, sale]: update field floor, make inverse field status, bulding in property, add avatar and change view of property

--- custom-addons/real-estate/models/property_template.py
+++ custom-addons/real-estate/models/property_template.py
@@ -1,11 +1,15 @@
+import base64
+
 from keras.src.utils import default
 
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
+from odoo.fields import Properties
 
 
 class RealEstateProperty(models.Model):
     _name = 'real.estate.property'
     _description = 'Property'
+    _order = "is_favorite desc"
 
     property = fields.Char('Property Code', required=True)
     property_name = fields.Char(string="Property", required=True, help="Name of the real estate property", default="")
@@ -18,6 +22,7 @@ class RealEstateProperty(models.Model):
         comodel_name='real.estate.property.status',
         string="Status",
         compute='_compute_status',
+        inverse="_inverse_status",
         store=True
     )
     description = fields.Char(string="Description", help="Description of the property")
@@ -32,11 +37,13 @@ class RealEstateProperty(models.Model):
         comodel_name='real.estate.building',
         string="Building",
         compute='_compute_building',
+        inverse="_inverse_building",
         store=True
     )
+    is_favorite = fields.Boolean(string="Favorite")
     total_area = fields.Float(string="Total Area (m²)", help="Total area of the property in square meters")
     property_value = fields.Float(string="Property Value", help="Total value of the property")
-    floor_number = fields.Integer(string="Floor Number", help="Floor number for properties within a building")
+    floor = fields.Char(string="Floor", help="Floor number for properties within a building")
     usable_area = fields.Float(string="Usable Area (m²)", help="Net usable area of the property")
     unit_price_excl_vat = fields.Float(string="Unit Price (Excl. VAT)", help="Unit price excluding VAT")
     unit_price_incl_vat = fields.Float(string="Unit Price (Incl. VAT)", help="Unit price including VAT")
@@ -48,6 +55,7 @@ class RealEstateProperty(models.Model):
         ('city', 'City View'),
         ('mountain', 'Mountain View')
     ], string="View", help="View type of the property")
+    image = fields.Binary(string="Property Image", attachment=True)
 
     @api.depends('status_code')
     def _compute_status(self):
@@ -58,6 +66,13 @@ class RealEstateProperty(models.Model):
                     record.status_id = status.id
                 else:
                     record.status_id = False
+
+    def _inverse_status(self):
+        for record in self:
+            if record.status_id:
+                record.status_code = record.status_id.status_code
+            else:
+                record.status_code = False
     @api.depends('building_code')
     def _compute_building(self):
         for record in self:
@@ -67,21 +82,22 @@ class RealEstateProperty(models.Model):
                     record.building_id = building.id
                 else:
                     record.building_id = False
+    def _inverse_building(self):
+        for record in self:
+            if record.building_id:
+                record.building_code = record.building_id.building_code
+            else:
+                record.building_code = False
+
+    @api.depends('property')
+    def _compute_display_name(self):
+        for property in self:
+            property.display_name =  property.property_name
     @api.model
     def default_get(self, fields_list):
         """Set default values for fields"""
         defaults = super(RealEstateProperty, self).default_get(fields_list)
         return defaults
-
-    def reserve_property(self):
-        """Reserve the property by setting its status to 'reserved'."""
-        # for record in self:
-        #     if record.status == 'available':
-        #         record.status = 'reserved'
-        #     else:
-        #         raise ValueError("Only available properties can be reserved.")
-        pass
-
     def compute_vat_prices(self):
         """Calculate VAT-inclusive and VAT-taxable values."""
         for record in self:
